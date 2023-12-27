f1d995dc0ef363bdc79e875d3da78ea6e15c91bc
ak <ak8893893@yahoo.com.tw>
2023-12-27 12:32:54 +0800

    31

---
+++ 01-彭彭課程/01-code/31-JavaScript Modules 模組的輸出和輸入/main.js
@@ -0,0 +1,24 @@
+// // 整合的輸入
+// import abc,{data,obj} from "./lib.js"
+
+// // 預設的輸入
+// // import abc from "./lib.js"
+
+// console.log(abc);
+
+// // 非預設的輸入
+// // import { data,obj } from "./lib.js";
+// console.log(data);
+// console.log(obj);
+
+
+
+
+
+
+// 練習
+import { multiply } from "./lib.js";
+multiply(3,6);
+
+import math from "./lib.js";
+math.add(8,7);
